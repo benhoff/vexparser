bf5d8c3c114df09588e25c8c7cff1db2ba01b445
Ben Hoff <hoff.benjamin.k@gmail.com>
2016-04-03 12:53:51 -0600

    initial project setup

---
+++ setup.py
@@ -0,0 +1,38 @@
+import os
+from setuptools import find_packages, setup
+
+
+# directory = os.path.abspath(os.path.dirname(__file__))
+"""
+with open(os.path.join(directory, 'README.rst')) as f:
+    long_description = f.read()
+"""
+
+setup(
+    name="commandparser",
+    version='0.0.1',
+    description='Command parsing for vexbot'
+    # long_description=long_description,
+    url='https://github.com/benhoff/commandparser',
+    license='GPL3',
+    classifiers=[
+        'Development Status :: 1 - Planning',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: GNU General Public License v3 (GPLv3)',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.5',
+        'Topic :: Utilities',
+        'Operating System :: OS Independent'],
+    keywords='command parsing intent classifier',
+    author='Ben Hoff',
+    author_email='beohoff@gmail.com',
+    packages= find_packages(), # exclude=['docs', 'tests']
+    install_requires=[
+        'pluginmanager',
+        'pyzmq'
+        ],
+
+    extras_require={
+        'dev': ['flake8']
+        },
+)
