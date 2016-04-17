1ad37a0a22d929f48c4dc3f99ad691c351fe5d0c
Ben Hoff <hoff.benjamin.k@gmail.com>
2016-04-16 19:04:19 -0600

    added in some reqs

--- setup.py
+++ setup.py
@@ -29,7 +29,10 @@ setup(
     packages= find_packages(), # exclude=['docs', 'tests']
     install_requires=[
         'pluginmanager',
-        'pyzmq'
+        'pyzmq',
+        'numpy',
+        'textblob',
+        'nltk'
         ],
 
     extras_require={
